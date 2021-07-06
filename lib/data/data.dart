6f424327862c15ef5be0b23cda0e0613bb3e8521
althierfson <althierfson@gmail.com>
2021-07-06 20:23:16 -0300

    Base do jogo da memmoria

---
+++ lib/data/data.dart
@@ -0,0 +1,125 @@
+import 'package:memory_game/model/tile_model.dart';
+
+int points = 1000;
+bool selected = false;
+String selectedImageAssetPath = "";
+int selectedTileIndex = 123;
+
+List<TileModel> pairs = [];
+List<TileModel> visiblePairs = [];
+
+List<TileModel> getPairs() {
+
+  List<TileModel> pairs = [];
+  TileModel tileModel;
+
+  //1
+  tileModel = new TileModel("assets/N0.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //2
+  tileModel = new TileModel("assets/N1.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //3
+  tileModel = new TileModel("assets/N2.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //4
+  tileModel = new TileModel("assets/N3.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //5
+  tileModel = new TileModel("assets/N4.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //5
+  tileModel = new TileModel("assets/N5.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //6
+  tileModel = new TileModel("assets/N6.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //7
+  tileModel = new TileModel("assets/N7.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //8
+  tileModel = new TileModel("assets/N8.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //9
+  tileModel = new TileModel("assets/N9.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  return pairs;
+}
+
+List<TileModel> getHideSide() {
+
+  List<TileModel> pairs = [];
+  TileModel tileModel;
+
+  //1
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //2
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //3
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //4
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //5
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //5
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //6
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //7
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //8
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  //9
+  tileModel = new TileModel("assets/hideSide.png", false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+
+  return pairs;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:memory_game/data/data.dart';
+import 'package:memory_game/model/tile_model.dart';
 // video base: https://www.youtube.com/watch?v=vfF0-hZaKNM
 
 void main() {
@@ -25,18 +27,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    pairs = getPairs();
+    pairs.shuffle();
+
+    visiblePairs = pairs;
+    selected = true;
+
+    Future.delayed(const Duration(seconds: 5), (){
+      setState(() {
+        visiblePairs = getHideSide();
+        selected = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
         child: Column(
           children: <Widget>[
-            Text("0/800"),
-            Text("Points"),
+            SizedBox(height: 25),
+            points != 1000 ? Column(
+              children: [
+                Text("$points/1000", style:TextStyle(
+                  fontSize: 24,
+                    fontWeight: FontWeight.w500
+                )),
+                Text("Points"),
+              ],
+            ) : Container(),
             SizedBox(height: 20, ),
-            GridView(
+            points != 1000 ? GridView(
               shrinkWrap: true,
-              gridDelegate: SliverGridDelegate,
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                mainAxisSpacing: 0.0, maxCrossAxisExtent: 100
+              ),
+              children: List.generate(visiblePairs.length, (index){
+                return Tile(
+                  imageAssetPath: visiblePairs[index].getImageAssetPath(),
+                  tileIndex: index,
+                  parent: this,
+                );
+              }),
+            ) : Container(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(24)
+              ),
+              child: Text("Replay", style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+                color: Colors.white
+              ),),
             )
           ],
         ),
@@ -45,3 +97,72 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class Tile extends StatefulWidget {
+
+  String imageAssetPath;
+  int tileIndex;
+  _HomePageState parent;
+  Tile({required this.imageAssetPath, required this.parent, required this.tileIndex});
+
+  @override
+  _TileState createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        if(!selected){
+          if(selectedImageAssetPath != ""){
+
+            if(selectedImageAssetPath == pairs[widget.tileIndex].getImageAssetPath()) {
+              print("Correct");
+
+              selectedImageAssetPath = "";
+              selected = true;
+
+              Future.delayed(const Duration(seconds: 2), (){
+                setState(() {  
+                  points = points + 100;
+                });
+                selected = false;
+                widget.parent.setState(() {
+                });
+              });
+            }else{
+              print("Wrong");
+
+              selectedImageAssetPath = "";
+              selected = true;
+
+              Future.delayed(const Duration(seconds: 2), () {
+                selected = false;
+                setState(() {
+                  //points = points - 50;
+                });
+                widget.parent.setState(() {
+                  pairs[widget.tileIndex].setIsSelected(false);
+                  pairs[selectedTileIndex].setIsSelected(false);
+                });
+              });
+            }
+
+          }else{
+            selectedTileIndex = widget.tileIndex;
+            selectedImageAssetPath = pairs[widget.tileIndex].getImageAssetPath();
+          }
+
+          setState(() {
+            pairs[widget.tileIndex].setIsSelected(true);
+          });
+          print("Virar carta");
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.all(5),
+        child: Image.asset(pairs[widget.tileIndex].getIsSelected() ? pairs[widget.tileIndex].getImageAssetPath() : widget.imageAssetPath)
+      ),
+    );
+  }
+}
